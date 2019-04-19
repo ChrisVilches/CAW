ce89b600dc96b1cdb5bfcc2025c4c4ab9711cf99
Felo Vilches <felovilches@gmail.com>
2019-04-20 03:26:32 +0900

    modal right

--- caw_client/js/index.js
+++ caw_client/js/index.js
@@ -18,8 +18,7 @@ $("#input-form").on("submit", function(ev){
     processData: false
   })
   .done(res => {
-    var template = $('#word-result-template').html();
-    var html = Mustache.to_html(template, res);
+    var html = getWords2HTML(res);
     $('#result').hide();
     $('#result').html(html);
     $('#result').fadeIn();
@@ -37,3 +36,70 @@ $('#input-form').keydown(function(event) {
     $(this).trigger('submit');
   }
 });
+
+$('#right-modal').modal({ show: false});
+
+function getWords2HTML(wordsArray){
+  var template = $('#word-result-template').html();
+  var html = Mustache.to_html(template, wordsArray);
+  return html;
+}
+
+function displayModal(modalTitle, modalContent, animation = false){
+  function animate(el){
+    if(!animation) return;
+
+    el.animate({ marginLeft: "90", opacity: 0 }, 0, function(){
+      el.animate({ marginLeft: "0", opacity: 1 }, 800);
+    });
+  }
+
+  function setBody(value, domId){
+    var el = $(domId);
+    if(typeof value === "function"){
+      el.html("Loading...");
+      value(val => {
+        el.html(val);
+        animate(el);
+      });
+    } else {
+      el.html(value);
+      animate(el);
+    }
+  }
+
+  $("#modal-title").html(modalTitle);
+  setBody(modalContent, "#modal-content");
+
+  var modalEl = $("#right-modal");
+
+  if(!(modalEl.data('bs.modal') || {})._isShown){
+    modalEl.modal('show');
+  }
+}
+
+function displayModalShowWord(word){
+
+  word = word.trim();
+
+  displayModal(word, function(cb){
+
+    $.ajax({
+      type: 'GET',
+      url: "http://www.felovilches.com/caw/api/words?q=" + word,
+      processData: false
+    })
+    .done(res => {
+      var html = getWords2HTML(res);
+      cb(html);
+    })
+    .catch(alert)
+    .done(() => {
+
+    });
+
+  }, true);
+
+
+
+}
