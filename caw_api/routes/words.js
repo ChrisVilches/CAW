febaf19d64d0962a0ac861bfc2734193e33eabd8
Chris Vilches (DigitalOcean droplet) <felovilches@gmail.com>
2021-09-06 07:00:39 +0000

    add auto tokenizer

--- caw_api/routes/words.js
+++ caw_api/routes/words.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var Word = require('../models/word');
+const axios = require('axios');
+
+const JAPANESE_TOKENIZER_ENDPOINT = 'http://127.0.0.1:45678/important_words'; // TODO: .env
+
+// Limit both full text char count, and individual words to be queried.
+const TEXT_CHAR_LIMIT = 256;
+const WORD_LIMIT = 10;
+
+const stringTake = (string, n) => {
+  return string.substr(0, n);
+}
+
+const arrayTake = (array, n) => {
+  return array.slice(0, n);
+};
 
 router.get('/', async (req, res) => {
+  let text = req.query.q || '';
+  text = stringTake(text, TEXT_CHAR_LIMIT);
 
-  wordsArray = (req.query.q || '').split(",");
+  const tokenized = await axios.post(JAPANESE_TOKENIZER_ENDPOINT, { text });
+  let wordsArray = tokenized.data.result;
+  wordsArray = arrayTake(wordsArray, WORD_LIMIT);
 
-  let word = await Word.find({ word: { $in: wordsArray } });
+  const result = await Word.find({ word: { $in: wordsArray } });
 
-  res.send(word);
+  res.send(result);
 });
 
 module.exports = router;
